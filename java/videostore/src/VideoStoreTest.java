ff81f5d756f0e0767164bca330c99ea090422ddc
jarkyn <jarkyn@ymail.com>
2015-12-15 23:42:25 +0000

    finds/replaces within file Cmd+r

--- java/videostore/src/VideoStoreTest.java
+++ java/videostore/src/VideoStoreTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertEquals;
 
 public class VideoStoreTest {
 
-    private final String newReleaseTitle = "The Cell";
-    private final String childrensTitle = "The Tigger Movie";
-    private final String regularTitle = "Eraserhead";
+    private final String newReleaseTitle = "New Release";
+    private final String childrensTitle = "Childrens";
+    private final String regularTitle = "Regular";
 
     @Before
     public void setUp() {
@@ -17,21 +17,21 @@ public class VideoStoreTest {
     @Test
     public void testSingleNewReleaseStatement() {
         customer.addRental(new Rental(new Movie(newReleaseTitle, Movie.NEW_RELEASE), 3));
-        assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement());
+        assertEquals("Rental Record for Fred\n\tNew Release\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.statement());
     }
 
     @Test
     public void testDualNewReleaseStatement() {
         customer.addRental(new Rental(new Movie(newReleaseTitle, Movie.NEW_RELEASE), 3));
         customer.addRental(new Rental(new Movie(newReleaseTitle, Movie.NEW_RELEASE), 3));
-        assertEquals("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Cell\t9.0\nYou owed 18.0\nYou earned 4 frequent " +
+        assertEquals("Rental Record for Fred\n\tNew Release\t9.0\n\tNew Release\t9.0\nYou owed 18.0\nYou earned 4 frequent " +
                 "renter points\n", customer.statement());
     }
 
     @Test
     public void testSingleChildrensStatement() {
         customer.addRental(new Rental(new Movie(childrensTitle, Movie.CHILDRENS), 3));
-        assertEquals("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement());
+        assertEquals("Rental Record for Fred\n\tChildrens\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.statement());
     }
 
     @Test
@@ -40,7 +40,7 @@ public class VideoStoreTest {
         customer.addRental(new Rental(new Movie(regularTitle, Movie.REGULAR), 2));
         customer.addRental(new Rental(new Movie(regularTitle, Movie.REGULAR), 3));
 
-        assertEquals("Rental Record for Fred\n\tEraserhead\t2.0\n\tEraserhead\t2.0\n\tEraserhead\t3.5\nYou owed 7" +
+        assertEquals("Rental Record for Fred\n\tRegular\t2.0\n\tRegular\t2.0\n\tRegular\t3.5\nYou owed 7" +
                 ".5\nYou " +
                 "earned 3 frequent renter points\n", customer.statement());
     }
