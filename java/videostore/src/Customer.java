a0da1a1676272a57697975ee90f6bfa0788a5d48
jarkyn <jarkyn@ymail.com>
2015-12-21 13:15:21 +0000

    extracts and distributes methods from Customer#statement

--- java/videostore/src/Customer.java
+++ java/videostore/src/Customer.java
@@ -18,46 +18,38 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
 
-        for ( Rental rental : rentals) {
-            double thisAmount = 0;
+        result += lineEntries();
 
-            // determines the amount for each line
-            switch (rental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (rental.getDaysRented() > 2)
-                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += rental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (rental.getDaysRented() > 3)
-                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            frequentRenterPoints++;
-
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
-                    && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
-
-            result += "\t" + rental.getMovie().getTitle() + "\t"
-                    + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
-
-
-        }
-
-        result += "You owed " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
+        result += "You owed " + getTotalAmount() + "\n";
+        result += "You earned " + totalFrequentRenterPoints() + " frequent renter points\n";
 
         return result;
+    }
+
+    private String lineEntries() {
+        String result = "";
+        for ( Rental rental : rentals) {
+            result += "\t" + rental.getMovie().getTitle() + "\t"
+                    + rental.rentalAmount() + "\n";
+        }
+        return result;
+    }
+
+    private double getTotalAmount() {
+        double totalAmount = 0;
+        for (Rental rental : rentals) {
+            totalAmount += rental.rentalAmount();
+        }
+        return totalAmount;
+    }
+
+    private int totalFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        for (Rental rental : rentals) {
+            frequentRenterPoints += rental.calculateFrequentRenterPoints();
+        }
+        return frequentRenterPoints;
     }
 }
--- java/videostore/src/Rental.java
+++ java/videostore/src/Rental.java
@@ -14,4 +14,33 @@ public class Rental {
 
     private Movie movie;
     private int daysRented;
+
+    int calculateFrequentRenterPoints() {
+        int frequentRenterPoints = 1;
+
+        if (getMovie().getPriceCode() == Movie.NEW_RELEASE
+                && getDaysRented() > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
+
+    double rentalAmount() {
+        double rentalAmount = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                rentalAmount += 2;
+                if (getDaysRented() > 2)
+                    rentalAmount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                rentalAmount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                rentalAmount += 1.5;
+                if (getDaysRented() > 3)
+                    rentalAmount += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return rentalAmount;
+    }
 }
